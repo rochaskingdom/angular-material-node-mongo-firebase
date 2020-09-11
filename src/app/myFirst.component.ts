3aa5474a99f83454ef3643727e5e3bfc0e24167b
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-09-10 22:18:09 -0300

    Estilos

--- src/app/myFirst.component.ts
+++ src/app/myFirst.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'myfirst',
-  templateUrl: './myFirst.component.html'
+  templateUrl: './myFirst.component.html',
+  styleUrls: ['./myFirst.component.css']
 })
 export class MyFirstComponent {
 
