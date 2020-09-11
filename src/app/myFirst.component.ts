18332e59ed0b9c5292b77ba540a6cd13bb718df8
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-09-10 22:22:06 -0300

    Seletores

--- src/app/myFirst.component.ts
+++ src/app/myFirst.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'myfirst',
+  selector: '.myfirst', // seletor de class
+  // selector: '[myfirst]', seletor de diretiva
   templateUrl: './myFirst.component.html',
   styleUrls: ['./myFirst.component.css']
 })
